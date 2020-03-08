bdd9c72e6a58f5fe98aa1fb68b6f38222c5dd073
Diego Santaolalla Martin <diegosantaolallamartin@gmail.com>
2020-03-08 15:46:42 +0100

    Changes in database schema

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import { createConnection, getConnection, getRepository } from 'typeorm';
 import { UserAccessEntity } from 'src/entity/user-access.entity';
+import { UserRoleEntity } from 'src/entity/user-roles.entity';
+import { UserToRoleEntity } from 'src/entity/user-to-role.entity';
 
 
 export class Database {
@@ -25,6 +27,14 @@ export class Database {
 
   public static async getUserAccessRespository(): Promise<any> {
     return getRepository(UserAccessEntity);
+  }
+
+  public static async getRolesRepository(): Promise<any> {
+    return getRepository(UserRoleEntity);
+  }
+
+  public static async getUserToRoleRepository(): Promise<any> {
+    return getRepository(UserToRoleEntity);
   }
 
 }
