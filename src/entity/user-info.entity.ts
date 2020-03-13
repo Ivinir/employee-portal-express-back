e1830b4d04b346b4dea2445a50bd20ccb9160050
Diego Santaolalla Martin <diegosantaolallamartin@gmail.com>
2020-03-13 20:01:11 +0100

    Added user-info entity

---
+++ src/entity/user-info.entity.ts
@@ -0,0 +1,38 @@
+import { UserAccessEntity } from 'src/entity/user-access.entity';
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn, OneToOne, Column } from 'typeorm';
+
+@Entity('user_info')
+export class UserInfoEntity {
+
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @OneToOne(
+    type => UserAccessEntity,
+    (userAccessEntity) => userAccessEntity.user_id)
+  @JoinColumn({ name: 'user_id' })
+  public user_id!: string;
+
+  @Column({
+    length: 100
+  })
+  public user_name!: string;
+
+  @Column({
+    length: 100
+  })
+  public user_surname1!: string;
+
+  @Column({
+    length: 100
+  })
+  public user_surname2!: string;
+
+  @Column({
+    length: 100
+  })
+  public user_address!: string;
+
+
+
+}
